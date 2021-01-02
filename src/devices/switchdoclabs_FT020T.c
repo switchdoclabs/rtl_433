b79d3eaef65d0376421a414818fdefeacb97ac47
mwaterbu <mwaterbu@users.noreply.github.com>
2021-01-02 12:36:50 -0500

    Update switchdoclabs_FT020T.c
    Normalize output between F016TH and FT020T sensors.
    Display temperature in degF, and show battery status as OK/Low.

--- src/devices/switchdoclabs_FT020T.c
+++ src/devices/switchdoclabs_FT020T.c
@@ -133,7 +133,8 @@ switchdoclabs_weather_decode(r_device *decoder, bitbuffer_t *bitbuffer, unsigned
    	myWindDirection= b2[4] | ((myFlags & 0x04)<<6);
    	myCumulativeRain=(b2[5]<<8) + b2[6]; 
    	mySecondFlags  = (b2[7] & 0xf0)>>4;
-	myTemperature = ((b2[7] & 0x0f)<<8) + b2[8];  
+	myTemperature = ((b2[7] & 0x0f)<<8) + b2[8];
+	float temperature = (myTemperature - 400) / 10.0f;
 	myHumidity = b2[9];
 	myLight = (b2[10]<<8) + b2[11] + ((mySecondFlags & 0x08)<<9);
 	myUV = b2[12]; 
@@ -192,12 +193,12 @@ switchdoclabs_weather_decode(r_device *decoder, bitbuffer_t *bitbuffer, unsigned
             "model",          "",             DATA_STRING, _X("SwitchDoc Labs-FT020T","SwitchDoc Labs FT020T AIO"),
             _X("id","device"),         "Device",   DATA_INT,    myDevice,
             "id",        "Serial Number",      DATA_INT,    mySerial,
-            "batterylow",        "Battery Low",      DATA_INT, myBatteryLow,
+            "battery",        "Battery",      DATA_STRING, myBatteryLow ? "Low" : "OK",
             "avewindspeed",        "Ave Wind Speed",      DATA_INT, myAveWindSpeed,
             "gustwindspeed",        "Gust",      DATA_INT, myGust,
             "winddirection",        "Wind Direction",      DATA_INT, myWindDirection,
             "cumulativerain",        "Cum Rain",      DATA_INT, myCumulativeRain,
-            "temperature",        "Temperature",      DATA_INT, myTemperature,
+            "temperature_F",  "Temperature",  DATA_FORMAT, "%.1f F", DATA_DOUBLE, temperature,
             "humidity",        "Humidity",      DATA_INT, myHumidity,
             "light",        "Light",      DATA_INT, myLight,
             "uv",        "UV Index",      DATA_INT, myUV,
